fc13d337e27a6609f0e3f04816cf16a8245c5848
Alex_Lomas <alex.lomaszewicz@gmail.com>
2025-04-01 13:57:44 -0400

    feat: Ajout du model Training

---
+++ src/main/java/ca/usherbrooke/trainingapi/model/Training.java
@@ -0,0 +1,98 @@
+package ca.usherbrooke.trainingapi.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Modèle représentant un entraînement.
+ */
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+@Entity
+public class Training {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private int time;
+    @ManyToOne
+    @JoinColumn(name = "discipline_id")
+    private Discipline discipline;
+    @OneToMany(mappedBy = "training", cascade = CascadeType.ALL)
+    private List<Exercice> exercices;
+
+    public Training() {
+        super();
+        this.discipline = null;
+        this.exercices = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<Exercice> getExercices() {
+        return exercices;
+    }
+
+    public void setExercices(List<Exercice> exercices) {
+        this.exercices = exercices;
+    }
+
+    public Discipline getDiscipline() {
+        return discipline;
+    }
+
+    public void setDiscipline(Discipline discipline) {
+        this.discipline = discipline;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Ajoute un exercice à l'entraînement.
+     *
+     * @param exercice l'exercice à ajouter
+     */
+    public void addExercice(Exercice exercice) {
+        this.exercices.add(exercice);
+    }
+
+    /**
+     * Supprime un exercice de l'entraînement.
+     *
+     * @param exercice l'exercice à supprimer
+     */
+    public void removeExercice(Exercice exercice) {
+        this.exercices.remove(exercice);
+    }
+}
