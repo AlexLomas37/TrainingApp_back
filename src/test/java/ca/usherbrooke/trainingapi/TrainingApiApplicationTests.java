a8ebac7a31f00b78b15e81dd72d56166b2e02e06
Alex_Lomas <alex.lomaszewicz@gmail.com>
2025-04-04 09:29:49 -0400

    refactor: Mise à jour des tests pour implémenter les nouveaux ajouts

--- src/test/java/ca/usherbrooke/trainingapi/TrainingApiApplicationTests.java
+++ src/test/java/ca/usherbrooke/trainingapi/TrainingApiApplicationTests.java
@@ -1,14 +1,12 @@
 package ca.usherbrooke.trainingapi;
 
-import ca.usherbrooke.trainingapi.model.Discipline;
-import ca.usherbrooke.trainingapi.model.Exercice;
-import ca.usherbrooke.trainingapi.model.Training;
-import ca.usherbrooke.trainingapi.repository.DisciplineRepository;
-import ca.usherbrooke.trainingapi.repository.ExerciceRepository;
-import ca.usherbrooke.trainingapi.repository.TrainingRepository;
+import ca.usherbrooke.trainingapi.model.*;
+import ca.usherbrooke.trainingapi.repository.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class TrainingApiApplicationTests {
@@ -19,6 +17,10 @@ class TrainingApiApplicationTests {
     private TrainingRepository trainingRepository;
     @Autowired
     private ExerciceRepository exerciceRepository;
+    @Autowired
+    private TrainingSessionRepository trainingSessionRepository;
+    @Autowired
+    private ExerciceSessionRepository exerciceSessionRepository;
 
     @Test
     public void testSaveExerciceSessionWithStatistic() {
@@ -42,10 +44,39 @@ class TrainingApiApplicationTests {
         exercice.setRepetitions(5);
         exercice.setTraining(training);
 
+        // Ajout des statistiques à l'exercice
+        exercice.addStatistic(StatisticType.SCORE, "points");
+        exercice.addStatistic(StatisticType.TIME, "s");
+        exercice.addStatistic(StatisticType.ACCURACY, "%");
+
         // Enregistrement des objets dans la base de données
         disciplineRepository.save(discipline);
         trainingRepository.save(training);
         exerciceRepository.save(exercice);
-    }
 
+        // Test creation d'une session d'entrainements
+        TrainingSession trainingSession = new TrainingSession();
+        trainingSession.setTraining(training);
+        trainingSession.setStart(LocalDateTime.now());
+        trainingSession.setEnd(LocalDateTime.now().plusMinutes(30));
+
+        // Enregistrement de la session d'entrainement
+        trainingSessionRepository.save(trainingSession);
+
+        // Test creation d'une session pour un exercice
+        ExerciceSession exerciceSession = new ExerciceSession();
+        exerciceSession.setExercice(exercice);
+        exerciceSession.setTrainingSession(trainingSession);
+        exerciceSession.setStart(LocalDateTime.now());
+        exerciceSession.setEnd(LocalDateTime.now().plusMinutes(10));
+
+        // Test Add Statistics for exercice session
+        /*ExerciceStatistic stat1 = new ExerciceStatistic();
+        stat1.setExerciceSession(exerciceSession);
+        stat1.setKeyStat(StatisticType.SCORE);
+        stat1.setValue("100");*/
+
+        // Enregistrement de la session d'exercice
+        exerciceSessionRepository.save(exerciceSession);
+    }
 }
