e8d9a34992aff32e0ce65a54d8cd6b5c366176ec
Alex_Lomas <alex.lomaszewicz@gmail.com>
2025-04-05 09:57:18 -0400

    feat: Ajout de la requete pour récupérer la liste de training session en fonction de l'id du training, de la date de début et de fin

--- src/main/java/ca/usherbrooke/trainingapi/repository/TrainingSessionRepository.java
+++ src/main/java/ca/usherbrooke/trainingapi/repository/TrainingSessionRepository.java
@@ -2,8 +2,14 @@ package ca.usherbrooke.trainingapi.repository;
 
 import ca.usherbrooke.trainingapi.model.TrainingSession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface TrainingSessionRepository extends JpaRepository<TrainingSession, Integer> {
 
     Iterable<TrainingSession> findByTrainingId(int training_id);
+
+    @Query("SELECT ts FROM TrainingSession ts WHERE ts.training.id = :trainingId AND ts.start BETWEEN :startDate AND :endDate")
+    Iterable<TrainingSession> findByDateTrainingSessionBetweenAndTrainingId(LocalDateTime startDate, LocalDateTime endDate, int trainingId);
 }
