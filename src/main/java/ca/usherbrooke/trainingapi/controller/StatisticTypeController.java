5ce3e6f70b4485ed5c3da7e3dea230cf7d2f1cae
Alex_Lomas <alex.lomaszewicz@gmail.com>
2025-04-05 09:57:36 -0400

    refactor: Modification des routes

--- src/main/java/ca/usherbrooke/trainingapi/controller/StatisticTypeController.java
+++ src/main/java/ca/usherbrooke/trainingapi/controller/StatisticTypeController.java
@@ -2,15 +2,17 @@ package ca.usherbrooke.trainingapi.controller;
 
 import ca.usherbrooke.trainingapi.model.StatisticType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/statistic-types")
 public class StatisticTypeController {
 
-    @GetMapping("/statistic-types")
+    @GetMapping
     public String getStatisticTypes() {
         return Arrays.stream(StatisticType.values())
                 .map(Enum::name)
