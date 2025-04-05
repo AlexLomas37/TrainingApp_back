cd89b207511ca31ee32569585bfe7d23fc833841
Alex_Lomas <alex.lomaszewicz@gmail.com>
2025-04-05 11:01:46 -0400

    feat: Ajout des requêtes pour récupérer la liste de ExerciceSession en fonction d'une période ou d'un nombre de fois précis

--- src/main/java/ca/usherbrooke/trainingapi/repository/ExerciceSessionRepository.java
+++ src/main/java/ca/usherbrooke/trainingapi/repository/ExerciceSessionRepository.java
@@ -2,8 +2,18 @@ package ca.usherbrooke.trainingapi.repository;
 
 import ca.usherbrooke.trainingapi.model.ExerciceSession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface ExerciceSessionRepository extends JpaRepository<ExerciceSession, Integer> {
 
-    Iterable<ExerciceSession> findByExerciceId(int idExo);
+    @Query("SELECT es FROM ExerciceSession es WHERE es.exercice.id = :idExercice")
+    Iterable<ExerciceSession> findByExerciceId(int exerciceId);
+
+    @Query("SELECT es FROM ExerciceSession es WHERE es.exercice.id = :exerciceId AND es.start BETWEEN :startDate AND :endDate")
+    Iterable<ExerciceSession> findByExerciceIdAndDates(int exerciceId, LocalDateTime startDate, LocalDateTime endDate);
+
+    @Query("SELECT es FROM ExerciceSession es WHERE es.exercice.id = :exerciceId ORDER BY es.start DESC LIMIT :nb")
+    Iterable<ExerciceSession> findByExerciceIdAndLastNbTime(int exerciceId, int nb);
 }
