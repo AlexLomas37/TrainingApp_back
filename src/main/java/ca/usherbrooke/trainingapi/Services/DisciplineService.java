d7def32f1d9ef8985343826ccd2621b7e7d2be7a
Alex_Lomas <alex.lomaszewicz@gmail.com>
2025-04-14 11:12:54 -0400

    feat: Modification de la méthode patch pour permettre la modification d'image

--- src/main/java/ca/usherbrooke/trainingapi/Services/DisciplineService.java
+++ src/main/java/ca/usherbrooke/trainingapi/Services/DisciplineService.java
@@ -77,6 +77,9 @@ public class DisciplineService {
         if(discipline.getTrainings() != null && !discipline.getTrainings().isEmpty()) {
             existingDiscipline.setTrainings(discipline.getTrainings());
         }
+        if(discipline.getImageLink() != null) {
+            existingDiscipline.setImageLink(discipline.getImageLink());
+        }
         return disciplineRepository.save(existingDiscipline);
     }
 
